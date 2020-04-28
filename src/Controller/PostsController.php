d1ed7237278bc04962e4272efc5c964242f7a3b6
Riot Nicolas <nico.riot@free.fr>
2020-04-28 16:09:59 +0200

    suppr com avec test id/session et role

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -42,4 +42,82 @@ class PostsController extends AbstractController
             'form'=> $form->createView()
         ]);
     }
+
+    /**
+     * @Route("/post/comment/{id}", name="comment_edit",methods ="GET|POST")
+     */
+    public function ModifComment(Comment $comment,Request $req,EntityManagerInterface $man)
+    {
+
+        //id user  et role du user connect pour le test de modification 
+        $userid = $this->getUser()->getId(); //recuperation du user
+        $userRole = $this->getUser()->getRoles();
+        //dd($userRole);
+
+        //id du post pour la redirection
+        $idPost = $comment->getPost()->getId();
+        //dd($idPost);
+
+        $form = $this->createForm(CommentType::class,$comment);
+        $form->handleRequest($req);
+
+
+        if( $userRole[0] == "ROLE_ADMIN" || $userid == $comment->getUser()->getId()){
+
+            if($form->isSubmitted() && $form->isValid()){
+    
+    
+                $man->persist($comment);
+                $man->flush();
+                return $this->redirectToRoute('post',['id' => $idPost]);
+                exit();
+    
+            }
+        }else{
+            return $this->redirectToRoute('post',['id' => $idPost]);
+        }
+
+
+
+
+        return $this->render('user/modifComment.html.twig',[
+            'comment' => $comment,
+            'form'=> $form->createView()
+        ]);
+    }  
+    
+    
+    /**
+     * @Route("/post/comment/{id}", name="comment_sup",methods="sup")
+     */
+    public function suprComment(Comment $comment,EntityManagerInterface $man,Request $req)
+    {
+        //id user  et role du user connect pour le test de modification 
+        $userid = $this->getUser()->getId(); //recuperation du user
+        $userRole = $this->getUser()->getRoles();
+
+
+        //id du post pour la redirection
+        $idPost = $comment->getPost()->getId();
+
+        if( $userRole[0] == "ROLE_ADMIN" || $userid == $comment->getUser()->getId()){
+
+            //test du token arg 1 sup+com id arg 2 dans la req recu du token
+            if($this->isCsrfTokenValid("sup".$comment->getId(), $req->get("_token"))){
+
+                $man->remove($comment);
+                $man->flush();
+                return $this->redirectToRoute('post',['id' => $idPost]);
+
+
+            }
+
+        }else{
+            return $this->redirectToRoute('post',['id' => $idPost]);
+        }
+    }
+
 }
+
+    
+        
