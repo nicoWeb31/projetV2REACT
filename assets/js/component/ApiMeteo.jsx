93c94a045f9411d5eab9ff8fe5b8577682887c59
Riot Nicolas <nico.riot@free.fr>
2020-05-26 21:34:48 +0200

    api meteo test

--- assets/js/component/ApiMeteo.jsx
+++ assets/js/component/ApiMeteo.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ApiMeteoDay from './ApiMeteoDay';
-import ReactCardCarousel from "react-card-carousel";
+
 
 
 
@@ -14,21 +14,6 @@ class ApiMeteo extends Component {
             lastResponse:null
         };
     }
-
-    static get CARD_STYLE() {
-        return {
-          height: "200px",
-          width: "200px",
-          paddingTop: "80px",
-          textAlign: "center",
-          background: "#52C0F5",
-          color: "#FFF",
-          fontSize: "12px",
-          textTransform: "uppercase",
-          borderRadius: "10px",
-        };
-      }
-
 
 
 
@@ -44,14 +29,14 @@ class ApiMeteo extends Component {
     clickSubmit = (e) => {
 
         e.preventDefault();
-        const url = "https://www.prevision-meteo.ch/services/json/";
+        const keyApi = '27d544efcc1c6328b73d495da2d3a53a'
         this.setState({loading:true})
 
-        fetch(url + this.state.ville)
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${this.state.ville}&appid=${keyApi}&lang=fr`)
         .then(res => res.json())
         .then(
         (result) => {
-        //console.log(result)
+        console.log(result)
             this.setState({
             loading:false,
             lastResponse:result,
@@ -73,6 +58,10 @@ class ApiMeteo extends Component {
 
     render() {
 
+        console.log(this.state.lastResponse && this.state.lastResponse)
+        console.log(this.state.lastResponse && this.state.lastResponse.list[0])
+
+
         return (
             <div>
                 <div className="form-group">
@@ -81,17 +70,18 @@ class ApiMeteo extends Component {
 
                 <button onClick={this.clickSubmit} className='btn btn-raised btn-primary m-3 p-3 mx-auto d-block'>Envoyer</button>
                 </div>
-                 {this.state.loading ? (
+                {this.state.loading ? (
                     <div className="spinner-border mx-auto" role="status">
                     <span className="sr-only mx-auto">Loading...</span>
-                  </div>
+                </div>
                 ):
                 (
-                    ["fcst_day_0","fcst_day_1","fcst_day_2","fcst_day_3"].map(ele =>
-                        <ReactCardCarousel autoplay={true} autoplay_speed={2500}>
-                            <ApiMeteoDay  key ={ele} data={this.state.lastResponse && this.state.lastResponse[ele]} style={ ApiMeteo.CARD_STYLE }/>
-                        </ReactCardCarousel>
+                    [1,8,16,24,32,40].map((ele,index) =>
+
+                            <ApiMeteoDay key={index}   data={this.state.lastResponse && this.state.lastResponse.list[ele]} />
+
                     )
+
                 )} 
             </div>
         );
--- assets/js/component/ApiMeteoDay.jsx
+++ assets/js/component/ApiMeteoDay.jsx
@@ -10,25 +10,29 @@ class ApiMeteoDay extends Component {
  
 
   render() {
+
+    console.log(this.props.data)
+
+
     return (
 
-      <div>
 
-            <div>
-            <img
-              src={this.props.data && this.props.data.icon}
-              alt={this.props.data && "icon_meteo"}
-            />
-            <p className="d-inline">
-              {this.props.data && this.props.data.day_long}
-            </p>
+
+            <>
+               <img
+                src = {this.props.data && `http://openweathermap.org/img/wn/${this.props.data.weather[0].icon}@2x.png`}
+                alt={this.props.data && "icon_meteo"}
+              /> 
+             {/* <p className="d-inline">
+                {this.props.data && this.props.data.list[this.props.key].dt_txt}
+              </p>  */}
   
-            </div>
+            </>
 
         
 
 
-      </div>
+  
 
 
     );
