c760d0f86c19d597db6228751c1060effd7e12b4
Riot Nicolas <nico.riot@free.fr>
2020-07-08 14:10:06 +0200

    test fonctionel

--- tests/AppBundle/Controller/CurrentPageTest.php
+++ tests/AppBundle/Controller/CurrentPageTest.php
@@ -5,27 +5,29 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CurrentPageTest  extends WebTestCase
 {
+
     /**
      * @dataProvider urlProvider
      */
     public function testRoute($url)
     {
+
+
         $client = static::createClient();
 
         $client->request('GET', $url);
 
         $this->assertResponseStatusCodeSame(200, $client->getResponse()->getStatusCode());
 
-        echo $client->getResponse()->getContent();
+        echo 'La page    "' . $url . '"   possede un status code "' . $client->getResponse()->getStatusCode() . '" ;';
+        echo "\n\n";
     }
-
 
 
     public function urlProvider()
     {
+        //devrait passer le test
         yield ['/'];
-        yield ['/logout'];
-
         yield ['/contact'];
         yield ['/login'];
         yield ['/register'];
@@ -33,12 +35,12 @@ class CurrentPageTest  extends WebTestCase
         yield ['/category/Trail'];
         yield ['/category/Trek'];
         yield ['/category/Vtt'];
+        yield ['/_Fragment-last-news'];
+
+        //page admin ne devrait pas repondre un satuts 200
+        yield ['/admin/post'];
+
 
     }
-
-
-
-
-    
 
 }
